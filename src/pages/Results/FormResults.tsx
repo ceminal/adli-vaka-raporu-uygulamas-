aa281fe67831f997d902a1b755ff77d65a91b8d6
Cem İnal <cem.inal@tcm.com.tr>
2024-04-25 17:42:00 +0300

    test

--- src/pages/Results/FormResults.tsx
+++ src/pages/Results/FormResults.tsx
@@ -9,6 +9,7 @@ import Footer from '../../layouts/Footer/Footer';
 import { IFormData } from '../../interfaces/formData';
 import { RootState } from '../../redux/store';
 
+
 const FormResults: React.FC = () => {
     const tableData = useSelector((state: RootState) => state.table.tableData)
     const navigate = useNavigate();
@@ -31,10 +32,10 @@ const FormResults: React.FC = () => {
         { text: '0-', value: '0 -' }
     ];
 
-    const tableColumns = [
-        { title: 'Ad', dataIndex: 'ad', key: 'ad' },
-        { title: 'Soyad', dataIndex: 'soyad', key: 'soyad' },
-        { title: 'Yaş', dataIndex: 'yas', key: 'yas' },
+    const tableColumns  = [
+        { title: 'Ad', dataIndex: 'ad' },
+        { title: 'Soyad', dataIndex: 'soyad'},
+        { title: 'Yaş', dataIndex: 'yas'},
         {
             title: 'Cinsiyet',
             dataIndex: 'cinsiyet',
@@ -43,7 +44,7 @@ const FormResults: React.FC = () => {
                 { text: 'Erkek', value: 'Erkek' },
                 { text: 'Kadın', value: 'Kadın' }
             ],
-            onFilter: (value: string, record: IFormData) => record.cinsiyet === value
+            // onFilter: (value: string, record: IFormData) => record.cinsiyet === value
         },
         {
             title: 'Kan Grubu',
@@ -51,13 +52,12 @@ const FormResults: React.FC = () => {
             key: 'kanGrubu',
             render: (_text: string, record: IFormData) => `${record.kanGrubu || ''} ${record.kanGrubuRh || ''}`,
             filters: bloodTypeFilters,
-            onFilter: (value: string, record: IFormData) => `${record.kanGrubu || ''} ${record.kanGrubuRh || ''}` === value
+            // onFilter: (value: string, record: IFormData) => `${record.kanGrubu || ''} ${record.kanGrubuRh || ''}` === value
 
         },
         {
             title: 'Geliş Nedeni',
             dataIndex: 'gelisNedeni',
-            key: 'gelisNedeni',
             filters: [
                 { text: 'Etkili Eylem', value: 'Etkili Eylem' },
                 { text: 'Trafik Kazası', value: 'Trafik Kazası' },
@@ -68,14 +68,13 @@ const FormResults: React.FC = () => {
                 { text: 'Zehirlenmeler', value: 'Zehirlenmeler' },
 
             ],
-            onFilter: (value: string, record: IFormData) => record.gelisNedeni === value
+            // onFilter: (value: string, record: IFormData) => record.gelisNedeni === value
         },
 
         { title: 'Geliş Nedeni Açıklama', dataIndex: 'gelisNedeniAciklama', key: 'gelisNedeniAciklama' },
         {
             title: 'Odada Bulunanlar',
             dataIndex: 'odadaBulunanlar',
-            key: 'odadaBulunanlar',
             render: (text: string[]) => (text.join(', ')),
             filters: [
                 { text: 'Tabip', value: 'Tabip' },
@@ -84,35 +83,33 @@ const FormResults: React.FC = () => {
                 { text: 'Refakatçi', value: 'Refakatçi' },
                 { text: 'Güvenlik Görevlisi', value: 'Güvenlik Görevlisi' },
             ],
-            onFilter: (value: string[], record: IFormData) => record.odadaBulunanlar === value
+            // onFilter: (value: string[], record: IFormData) => record.odadaBulunanlar === value
         },
         {
             title: 'Darp Durumu',
             dataIndex: 'darpDurumu',
-            key: 'darpDurumu',
             render: (text: string) => (text ? 'Evet' : 'Hayır'),
             filters: [
                 { text: 'Evet', value: 'Evet' },
                 { text: 'Hayır', value: 'Hayır' },
             ],
-            onFilter: (value: string, record: IFormData) => record.darpDurumu === value
+            // onFilter: (value: string, record: IFormData) => record.darpDurumu === value
         },
-        { title: 'Organizasyon', dataIndex: 'organizasyon', key: 'organizasyon' },
-        { title: 'Şikayet', dataIndex: 'sikayet', key: 'sikayet' },
-        { title: 'Doktor Adı', dataIndex: 'doktorAdi', key: 'doktorAdi' },
+        { title: 'Organizasyon', dataIndex: 'organizasyon'},
+        { title: 'Şikayet', dataIndex: 'sikayet'},
+        { title: 'Doktor Adı', dataIndex: 'doktorAdi' },
         {
             title: 'Uygun Ortam Sağlandı',
             dataIndex: 'uygunOrtamSaglandi',
-            key: 'uygunOrtamSaglandi',
-            render: (text: boolean) => (text ? 'Evet' : 'Hayır'),
+            render: (text: string) => (text ? 'Evet' : 'Hayır'),
             filters: [
                 { text: 'Evet', value: true },
                 { text: 'Hayır', value: false }
             ],
-            onFilter: (value: boolean, record: IFormData) => record.uygunOrtamSaglandi === value,
+            // onFilter: (value: string | boolean, record: IFormData) => `${record.uygunOrtamSaglandi}` === value,
         },
         {
-            title: '', key: 'action',
+            title: '',
             render: (record: IFormData) => (
                 <Button type="primary" danger onClick={() => handleDeleteRow(record.id)}>
                     Sil
